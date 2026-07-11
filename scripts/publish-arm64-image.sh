95f0e800b54e6e94e2d9b887e90e72280c7090d0
Makima04 <makima04@outlook.com>
2026-07-11 09:47:34 +0800

    ci: build amd64 image natively

---
+++ scripts/publish-arm64-image.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+remote="$(git config --get remote.origin.url)"
+case "$remote" in
+  git@github.com:*) repository="${remote#git@github.com:}" ;;
+  https://github.com/*) repository="${remote#https://github.com/}" ;;
+  *) echo "Unsupported origin remote: $remote" >&2; exit 1 ;;
+esac
+
+repository="${repository%.git}"
+repository="$(printf '%s' "$repository" | tr '[:upper:]' '[:lower:]')"
+image="ghcr.io/$repository"
+sha="$(git rev-parse HEAD)"
+
+docker buildx build --platform linux/arm64 \
+  --tag "$image:arm64-latest" \
+  --tag "$image:arm64-$sha" \
+  --push .
+
+docker buildx imagetools create \
+  --tag "$image:latest" \
+  --tag "$image:$sha" \
+  "$image:amd64-$sha" \
+  "$image:arm64-$sha"
